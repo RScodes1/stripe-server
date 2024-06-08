15856aa169e9286095a1035b966c944786025a64
rajcodes168h <raj19all@gmail.com>
2024-06-08 16:07:32 +0530

    routes defined for paymeny intents #1

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 require('dotenv').config()
-
+const bodyParser = require('body-parser');
 const app = express()
 
 const PORT = process.env.PORT;
+
+const {paymentRoute} = require('./routes/payment');
+
+app.use('/api/v1', paymentRoute);
+app.use(bodyParser.json());
 
 app.get('/',(req,res)=>{
     res.send({msg: "Hello Beautiful"});
---
+++ routes/payment.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const paymentRoute = express.Router()
+require('dotenv').config() // environment variables
+
+const stripe = require('stripe')(process.env.STRIPE_ACCESS_KEY);
+
+paymentRoute.post('/create_intent',async(req,res)=>{
+    try {
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: 2000, // Amount in cents
+            currency: 'usd', 
+        });
+        res.status(201).send({ paymentIntent }); // successful
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+})
+
+paymentRoute.post('/capture_intent/:id',async(req,res)=>{
+    try {
+        const { id } = req.params;
+        const paymentIntent = await stripe.paymentIntents.capture(id);
+        res.status(200).send({ paymentIntent });
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+})
+
+paymentRoute.post('/create_refund/:id',async(req,res)=>{
+    try {
+        const { id } = req.params;
+        const refund = await stripe.refunds.create({ payment_intent: id });
+        res.status(201).send({ refund });
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+})
+
+paymentRoute.get('./get_intents',async(req,res)=>{
+    try {
+        const paymentIntents = await stripe.paymentIntents.list({ limit: 10 });
+        res.status(200).send({ paymentIntents });
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+})
+
+module.exports = {
+    paymentRoute
+}
